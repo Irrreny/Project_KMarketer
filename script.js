82bbf96905a80d29bb7adca2d8fa94a7fecfb04e
Irina <tretiakovairyna@gmail.com>
2023-09-06 12:59:57 +0300

    created new test file

---
+++ script.js
@@ -0,0 +1,5 @@
+let country = `United Kingdom`;
+let continent = `Europe`;
+let population = 68;
+let isisland = true;
+let countryDescription = `${country}` is an ${isisland} and located in ${continent}, ${population} million people live there.;
