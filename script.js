b8fb294819658e490f88bb6baf562e389d7d8098
Irina <tretiakovairyna@gmail.com>
2023-09-06 13:10:13 +0300

    added console message

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let country = `United Kingdom`;
 let continent = `Europe`;
 let population = 68;
-let isisland = true;
-let countryDescription = `${country}` is an ${isisland} and located in ${continent}, ${population} million people live there.;
+let isIsland = true;
+let countryDescription = `The ${country} is located in ${continent}, ${population} million people live there.`;
+console.log(countryDescription);
