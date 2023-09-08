160484dd1070247f07ef44736e1277efd28471f3
Irina <tretiakovairyna@gmail.com>
2023-09-08 15:02:44 +0300

    added sarah bmi

--- script.js
+++ script.js
@@ -16,5 +16,12 @@ let weightSarah = 65;
 let weightBob = 93;
 let heightSarah = 1.75;
 let heightBob = 1.89;
+
 let sarahBMI = weightSarah / heightSarah ** 2;
 console.log(sarahBMI);
+if (sarahBMI < 18.5) {
+    console.log(`Sarah is underweight.`)
+}
+
+let bobBMI = weightBob / heightBob ** 2;
+console.log(bobBMI);
