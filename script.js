f8fed658787aaa844b4a625c0e1dcc24dd1e06ed
Irina <tretiakovairyna@gmail.com>
2023-09-11 16:44:48 +0300

    homework3

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log(isIsland);
 let countryDescription = `The ${country} is located in ${continent}, ${population} million people live there.`;
 console.log(countryDescription); */
 
-//Home Work 2//
+/*Home Work 2
 
 let weightSarah = 65;
 let weightBob = 93;
@@ -33,4 +33,88 @@ if (bobBMI < 18.5 || bobBMI > 25) {
 }
 else {
     console.log(`Bob's weight is normal.`)
+}*/
+
+//Home Work 3//
+
+let number = 8;
+console.log(number % 2 ? `odd` : `even`)
+
+let isLoggedIn = true;
+console.log(isLoggedIn ? `Hello again 🙃` : `Who are you?`)
+
+let iphone = 1000;
+let numberIphone = 25;
+let isAvailable = true;
+console.log(isAvailable ? `${iphone * numberIphone} euros`: `Sorry, this item is out of stock`)
+
+let year = 2023;
+console.log(year % 4==0 ? `2023 is a Leap Year` : `2023 is not a Leap Year`)
+
+let country = `is`;
+switch (country) {
+    case `md`:
+        console.log(`Republic of Moldova`)
+        break
+    case `ua`:
+        console.log(`Ukraine`)
+        break
+    case `uk`:
+        console.log(`United Kingdom`)
+        break
+    case `de`:
+        console.log(`Germany`)
+        break
+    case `is`:
+        console.log(`Iceland`)
+        break
+    default:
+        console.log(`This country code doesn't exist`)
 }
+
+let grade = 45;
+switch (true) {
+    case grade >=97 && grade <=100 : 
+        console.log(`Congrats, you have an A+`)
+        break
+    case grade >=90 && grade <=96 : 
+        console.log(`You have an A`)
+        break
+    case grade >=80 && grade <=89 : 
+        console.log(`You have a B`)
+        break
+    case grade >=70 && grade <=79 : 
+        console.log(`You have a C`)
+        break
+    case grade >=60 && grade <=69 : 
+        console.log(`You have a D`)
+        break
+    case grade >=20 && grade <=59 : 
+        console.log(`You Failed the exam`)
+        break
+    default:
+    console.log (`you didn't score a single point!`)
+}
+
+function tellFortune (n, z, y, x) {
+console.log(`You will be ${x} in ${y}, and will get married to ${z} with ${n} kids`)
+}
+tellFortune (1, `Dmitri`, `New York`, `a CTO`)
+tellFortune (2, `John`, `Lisbon`, `an Artist`)
+tellFortune (3, `Ryan`, `London`, `a Photographer`)
+
+function calculatedDogAge (age) {
+    let dogAge = age * 7;
+    console.log(`Your dog is ${dogAge} years old in Dog Years.`)
+}
+calculatedDogAge (1)
+calculatedDogAge (6)
+calculatedDogAge (12)
+
+const calculationSupply = (age, foodKg, maxYears=100) => {
+    const result = (maxYears-age)*foodKg*365
+console.log(`You will need ${result} kg of food to survive till you're ${maxYears} years old.` )
+}
+calculationSupply (35, 1, 90)
+calculationSupply (25, 2, 80)
+calculationSupply (30, 1.5)
